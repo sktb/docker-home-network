262d8392315b7a8087f9f738d650f835003ff1c0
Simon K T Burrows <sktburrows@gmail.com>
2016-03-15 21:30:26 +0000

    Remove build.sh as it's not needed using the new base resin base image.

--- build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf docker-arch-pi
-git clone https://github.com/sktb/docker-arch-pi.git
-cd docker-arch-pi
-./build.sh
-cd ../named
-docker build -t sktb/named .
-cd ../dhcpd
-docker build -t sktb/dhcpd .
